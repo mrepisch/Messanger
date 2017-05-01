00d06316c5e8266ca3628c4b016dbed8b7d5f488
PutriGrammer <petar.barisic2002@gmail.com>
2017-05-01 09:21:26 +0200

    Layouting. Anlegen der Activitys

--- app/src/main/java/main/nerd/messenger/Chat.java
+++ app/src/main/java/main/nerd/messenger/Chat.java
@@ -1,4 +1,13 @@
 package main.nerd.messenger;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Chat {
+
+    private ArrayList<Messages> messages = new ArrayList<Messages>();
+    private ArrayList<String> users = new ArrayList<String>();
+
+
+
 }
---
+++ app/src/main/java/main/nerd/messenger/Messages.java
@@ -0,0 +1,4 @@
+package main.nerd.messenger;
+
+class Messages {
+}
