312fc901e8b5cf12692c16303e6fd064f9b16298
bblans <mrepisch@hotmail.com>
2017-05-01 09:02:44 +0200

    Socket + Loginscreen

--- app/src/main/java/main/nerd/messenger/MainActivity.java
+++ app/src/main/java/main/nerd/messenger/MainActivity.java
@@ -2,7 +2,12 @@ package main.nerd.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import tcp.nerd.messenger.MessengerTcpSocket;
 
@@ -12,14 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MessengerTcpSocket a_messengerSocket = new MessengerTcpSocket(this);
-        a_messengerSocket.start();
+        final Button a_loginButton =(Button) findViewById(R.id.loginbtn);
+        a_loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText a_userNameEdit = (EditText)findViewById(R.id.username);
+                EditText a_passwordEdit = (EditText)findViewById(R.id.password);
+                String a_username = a_userNameEdit.getText().toString();
+                String a_password = a_passwordEdit.getText().toString();
+                SocketController.getInstance().getSocket().sendMessage("Login:username:"+a_username+":pw:"+a_password);
+                while(SocketController.getInstance().gethasMsgs() == false)
+                {
+
+                }
+                a_userNameEdit.setText(SocketController.getInstance().getReceivtMessages().get(0));
+                /*for( int i = 0; i < a_response.size(); i++)
+                {
+                    if( a_response.get(i) == "true")
+                    {
+                        boolean test = false;
+                        String somthign = "asd";
+                    }
+                }*/
+
+            }
+        });
     }
 
     public synchronized  void setText(String text)
     {
-        TextView a_text =(TextView) findViewById(R.id.msg);
-        a_text.setText(text);
+
 
     }
 }
--- app/src/main/java/tcp/nerd/messenger/MessengerTcpSocket.java
+++ app/src/main/java/tcp/nerd/messenger/MessengerTcpSocket.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -12,10 +13,11 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import main.nerd.messenger.MainActivity;
+import main.nerd.messenger.SocketController;
 
-import static main.nerd.messenger.R.id.msg;
 
 /**
  * Created by bblans on 25.04.2017.
@@ -29,12 +31,25 @@ public class MessengerTcpSocket extends Thread{
 
     private Socket m_socket;
 
-    private MainActivity m_act;
 
-    public MessengerTcpSocket(MainActivity t_act)
+
+    private BufferedReader m_reader;
+
+    private PrintWriter m_writer;
+
+    private boolean m_isRunning;
+
+    ArrayList<String>m_messagesToSend;
+
+    ArrayList<String>m_receivt;
+
+    public MessengerTcpSocket()
     {
         super("socket");
-        m_act = t_act;
+
+        m_messagesToSend = new ArrayList<String>();
+        m_receivt = new ArrayList<String>();
+        m_isRunning = true;
     }
 
 
@@ -46,7 +61,6 @@ public class MessengerTcpSocket extends Thread{
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-
         Log.e("TCP Client", "C: Connecting...");
 
         //create a socket to make the connection with the server
@@ -58,27 +72,81 @@ public class MessengerTcpSocket extends Thread{
 
         PrintWriter a_writer = null;
         try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(m_socket.getInputStream()));
-            a_writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(m_socket.getOutputStream())), true);
-            a_writer.println("HALLO SERVER");
-            a_writer.flush();
-            String fromServer = null;
-            while ((fromServer = input.readLine()) != null) {
-                m_act.setText(fromServer);
-            }
+
+            m_reader = new BufferedReader(new InputStreamReader(m_socket.getInputStream()));
+            m_writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(m_socket.getOutputStream())), true);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public synchronized  void sendMessage(String t_msg)
+    {
+        m_messagesToSend.add(t_msg);
+    }
 
+    public synchronized ArrayList<String>getReceivedList()
+    {
+        return m_receivt;
+    }
 
+    public void closeConnection()
+    {
+        try {
+            m_socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized ArrayList<String>getReceivt()
+    {
+        return m_receivt;
+    }
 
     public void run()
     {
         connect();
+        while(m_isRunning)
+        {
+            for( int i = 0; i < m_messagesToSend.size(); i++)
+            {
+                m_writer.println(m_messagesToSend.get(i));
+                m_writer.flush();
+                m_messagesToSend.clear();
+            }
+            try {
+                /*if(m_socket.getInputStream().available() > 0) {
+                    String fromServer = null;
+                    while ((  )!= null) {
+                        fromServer =  m_reader.readLine()
+                        m_receivt.add(fromServer);
+                    }*/
+                while( m_socket.getInputStream().available() > 0 )
+                {
+                    String fromServer = m_reader.readLine();
+                    if( fromServer.length() > 0) {
+                        m_receivt.add(fromServer);
+                        SocketController.getInstance().setHasMsgs(true);
+                    }
+
+                }
+
+                    sleep(10);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean test = true;
+        }
+        closeConnection();
     }
 
 
 }
+/*
+
+*/
