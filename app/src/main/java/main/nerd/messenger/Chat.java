4594f312891dc2cb63c04ae0155fa1cc29005048
PutriGrammer <petar.barisic2002@gmail.com>
2017-05-01 09:06:14 +0200

    Layout angefangen

---
+++ app/src/main/java/main/nerd/messenger/Chat.java
@@ -0,0 +1,4 @@
+package main.nerd.messenger;
+
+public class Chat {
+}
---
+++ app/src/main/java/main/nerd/messenger/ChatListActivity.java
@@ -0,0 +1,44 @@
+package main.nerd.messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addChatsToList();
+    }
+
+    private void addChatsToList() {
+        ListView chats = (ListView) findViewById(R.id.chatliste);
+        final ArrayList<Chat> allChats = new ArrayList<Chat>();
+        for (Chat b : allChats) {
+
+        }
+        chats.setAdapter(chatliste);
+        //Definition einer anonymen Klicklistener Klasse
+        AdapterView.OnItemClickListener mListClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                String selected = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(MainActivity.this, selected, Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        };
+
+        chats.setOnItemClickListener(mListClickedHandler);
+    }
+}
